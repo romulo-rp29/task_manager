f16b2b0027e2849aeb9dd27c83c8b043eef1c5eb
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-01-06 00:39:03 -0300

    feat(models): :sparkles: adicionar modelo Task
    Adicionado o modelo Task com campos title, description, completed, created_at, due_date e user. O modelo será usado para gerenciar tarefas diárias.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=None, blank=True, null=True)
