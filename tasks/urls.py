6e685288b43cd7585a70a58aac3539e69cb916c4
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-01-07 16:32:48 -0300

    feat(url): :sparkles: adicionadas urls para listagem e criação de tarefas

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,8 @@ from tasks import views
 
 urlpatterns = [
     path("", index, name="home-page"),
+    path("tasks/", views.tasks_list, name="tasks_list"),
+    path("create/", views.create_task, name="create_task"),
     path("task/<int:task_id>/edit/", views.edit_task, name="edit_task"),
     path("task/<int:task_id>/delete/", views.delete_task, name="delete_task"),
     path(
