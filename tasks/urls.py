3b05a92230e0cef51f12b9d8483dca2165de66a3
Rômulo Rodrigues <romulo.rp29@gmail.com>
2025-01-07 00:50:27 -0300

    feat(url): :sparkles: criação de urls para novas funçoes para tarefas
    foram criadas urls para deletar, editar e mudar tarefa para "concluída" ou "não concluída"

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from tasks.views import index
+from tasks import views
 
 
-urlpatterns = [path("", index, name="home-page")]
+urlpatterns = [
+    path("", index, name="home-page"),
+    path("task/<int:task_id>/edit/", views.edit_task, name="edit_task"),
+    path("task/<int:task_id>/delete/", views.delete_task, name="delete_task"),
+    path(
+        "task/<int:task_id>/toggle/",
+        views.toggle_task_completion,
+        name="toggle_task_completion",
+    ),
+]
